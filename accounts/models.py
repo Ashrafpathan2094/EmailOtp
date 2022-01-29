7c9a86b7d70a403cd37f2b7f5caaf2a2c054fcae
Ashrafpathan2094 <minazpathan2094@gmail.com>
2022-01-29 13:28:49 +0530

    Changes to be committed:

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
+from hashlib import md5
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    is_verified = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    auth_token = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.user.username
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from accounts.models import Profile
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import *
+import uuid
+from django.conf import settings
+from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def home(request):
+    return render(request , 'home.html')
